15b0894cf7ce3f437a1e2d71b07b97ad66eed859
Bobby Connolly <bobbyconnolly@gmail.com>
2023-02-03 18:38:45 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import { App } from "./App"
 import { DialogProvider } from "./dialog.provider"
 import "./index.css"
 
-// Be sure to wrap your app in the DialogProvider or else the dialog functionality won't work
+// Be sure to add an instance of DialogProvider or else the dialog functionality won't work
+// Here is a good place to put it. It's not necessary to wrap App in this case since we aren't using `React.createContext()`...
+// ... DialogProvider could be a sibling of <App /> and it would still work. Maybe there's a "better" way ¯\_(ツ)_/¯
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
